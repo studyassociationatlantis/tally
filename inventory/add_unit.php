f2c3c6fd4f6146d8aafc89efe0da2e86deb5eed3
robzelluf <robzelluf@hotmail.com>
2018-08-29 14:31:19 +0200

    Fixing inventory panel

--- inventory/add_unit.php
+++ inventory/add_unit.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 
 session_start();
 $servername = "localhost";
-$dbname = "saatla1q_tally";
+$dbname = "saatlant_tally";
 $username = $_SESSION['username'];
 $password = $_SESSION['password'];
 
