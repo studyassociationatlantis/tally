8e3baa644028c7f3cac4d6d51158132e15a962fe
robzelluf <robzelluf@hotmail.com>
2018-09-09 22:23:51 +0200

    Added comments

--- index.php
+++ index.php
@@ -23,11 +23,14 @@ if (isset($_SESSION['session_SN'])) {
 
 <body>
 
+<!--- Login form -->
 <form action="login.php" method="post" id="login_form">
     Student/employee number: <input id="session_SN" name="session_SN" type="text"><br>
     Password: <input id="pass" name="password" type="password"><br>
     <input id="submit" type="submit" value="Submit"><br>
 </form>
+
+
 <a href="https://sa-atlantis.nl/members-2/">Forgot password?</a><br>
 <br>
 By logging in, you accept that all purchases will be logged under your name. Please use this tally list system properly, and close it after you are finished. <br>
