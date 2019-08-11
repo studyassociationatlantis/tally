a3599d33ba329ba9a355e1a97707881a924e2b56
robzelluf <robzelluf@hotmail.com>
2019-08-11 16:41:14 +0200

    Add user if it is not in tally_users yet

--- purchases/settings.php
+++ purchases/settings.php
@@ -14,7 +14,14 @@ function change_settings($user, $student_number, $student_card) {
   $student_number = isset($student_number) ? 1 : 0;
   $student_card = isset($student_card) ? 1 : 0;
 
-  $sql = 'UPDATE '.$table.' SET sn_checkout = "'.$student_number.'", card_checkout = "'.$student_card.'" WHERE student_number = "'.$user.'"';
+  $sql = 'SELECT * FROM '.$table.' WHERE student_number = "'.$user.'"';
+  $result = $conn->query($sql);
+
+  if ($result->num_rows == 1) {
+    $sql = 'UPDATE '.$table.' SET sn_checkout = "'.$student_number.'", card_checkout = "'.$student_card.'" WHERE student_number = "'.$user.'"';
+  } else {
+    $sql = 'INSERT INTO '.$table.' (student_number, sn_checkout, card_checkout) VALUES ("'.$user.'", "'.$student_number.'", "'.$student_card.'")';
+  }
 
   if($conn->query($sql) == TRUE) {
     echo 'Updating settings successful!';
